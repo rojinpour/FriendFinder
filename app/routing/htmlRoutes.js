d59442482a345dd1aa33954b7681efeb65f4390c
Rojin Pourkhomami <Rojin@rojins-mbp.home>
2019-01-08 17:21:51 -0500

    added file

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,17 +1,6 @@
-
-// Pull in required dependencies
 var path = require('path');
+	module.exports = function(app) {
 
-// Export HTML routes
-module.exports = function(app) {
-	// console.log('___ENTER htmlRoutes.js___');
-
-	// Home page
-	app.get('/', function(req, res) {
-		res.sendFile(path.join(__dirname, '../public/home.html'));
-	});
-
-	// Survey page
 	app.get('/survey', function(req, res) {
 		res.sendFile(path.join(__dirname, '../public/survey.html'));
 	});
